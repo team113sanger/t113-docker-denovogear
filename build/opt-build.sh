a51a023edb453c232a7d959440d7cfae805d8cab
Victoria Offord <vo1@sanger.ac.uk>
2020-01-21 15:26:05 +0000

    initial setup

---
+++ build/opt-build.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+
+set -xe
+
+if [[ -z "${TMPDIR}" ]]; then
+  TMPDIR=/tmp
+fi
+
+set -u
+
+if [ "$#" -lt "1" ] ; then
+  echo "Please provide an installation path such as /opt/ICGC"
+  exit 1
+fi
+
+# get path to this script
+SCRIPT_PATH=`dirname $0`;
+SCRIPT_PATH=`(cd $SCRIPT_PATH && pwd)`
+
+# get the location to install to
+INST_PATH=$1
+mkdir -p $1
+INST_PATH=`(cd $1 && pwd)`
+echo $INST_PATH
+
+# get current directory
+INIT_DIR=`pwd`
+
+CPU=`grep -c ^processor /proc/cpuinfo`
+if [ $? -eq 0 ]; then
+  if [ "$CPU" -gt "6" ]; then
+    CPU=6
+  fi
+else
+  CPU=1
+fi
+echo "Max compilation CPUs set to $CPU"
+
+SETUP_DIR=$INIT_DIR/install_tmp
+mkdir -p $SETUP_DIR/distro # don't delete the actual distro directory until the very end
+mkdir -p $INST_PATH/bin
+cd $SETUP_DIR
+
+# make sure tools installed can see the install loc of libraries
+set +u
+export LD_LIBRARY_PATH=`echo $INST_PATH/lib:$LD_LIBRARY_PATH | perl -pe 's/:\$//;'`
+export LIBRARY_PATH=`echo $INST_PATH/lib:$LIBRARY_PATH | perl -pe 's/:\$//;'`
+export C_INCLUDE_PATH=`echo $INST_PATH/include:$C_INCLUDE_PATH | perl -pe 's/:\$//;'`
+export PATH=`echo $INST_PATH/bin:$PATH | perl -pe 's/:\$//;'`
+export MANPATH=`echo $INST_PATH/man:$INST_PATH/share/man:$MANPATH | perl -pe 's/:\$//;'`
+export PERL5LIB=`echo $INST_PATH/lib/perl5:$PERL5LIB | perl -pe 's/:\$//;'`
+set -u
+
+# DeNovoGear
+if [ ! -e $SETUP_DIR/denovogear.success ]; then
+  curl -L --retry 10 -o dist.tar.gz https://downloads.sourceforge.net/project/denovogear/Executables/Linux_64_x86/denovogear-${VER_DENOVOGEAR}-Linux-x86_64.tar.gz
+  mkdir denovogear
+  tar --strip-components 1 -C denovogear -xzf dist.tar.gz
+  mv  denovogear/bin/denovogear $INST_PATH/bin/denovogear
+  rm -rf dist.tar.gz denovogear*
+  touch $SETUP_DIR/denovogear.success
+fi
